a7fb86acf3a85176d3cbc3bc9bbc7b80b7aca870
diegobermudez09 <diegobl9do@gmail.com>
2024-07-05 18:27:52 -0500

    I'm adding the sample of the secctions and the interaction with the API FROM JSON_SERVER

---
+++ js/app.js
@@ -0,0 +1,118 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+};
+
+const categorias = {
+    1: 'Comida',
+    2: 'Bebida',
+    3: 'Postre',
+}
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    // Revisar si hay campos vacios, convertimos en arreglo a mesa y hora y utilizamos el arrayMethod
+    const camposVacios = [mesa, hora].some(campo => campo === ''); // some revisa si por lo menos uno de los campos esta vacio
+
+    // Verificar si ya hay una alerta
+    const existeAlerta = document.querySelector('.invalid-feedback');
+
+    if (camposVacios) {
+        if (!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+        return;
+    }
+
+    // Asignar datos del formulario al cliente
+    cliente = { ...cliente, mesa, hora };
+
+    // Ocultar Modal with Boostrap
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario); // le pasamos la instancia del formulario
+    modalBootstrap.hide();
+
+    // Mostrar las secciones
+    mostrarSecciones();
+
+    // Obtener Platillos de la API de JSON-Server
+    obtenerPlatillos();
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarPlatillos(resultado))
+        .catch(error => console.log(error));
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido');
+
+    // Iteramos en cada uno de los valores de la API en JSON
+    platillos.forEach(platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3', 'fw-bold');
+        categoria.textContent = categorias[platillo.categoria];
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.value = 0;
+        inputCantidad.min = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+        // Funcion que detecta la cantidad y el platillo que se esta agregando
+        inputCantidad.onchange = function () {
+            const cantidad = parseInt(inputCantidad.value);
+            agregarPlatillo({ ...platillo, cantidad }); // lo convierte a objeto para pasar ambos valores
+            // se coloca el spredOperator. para que me lo entregue todo como un objeto. y no por separado
+        };
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+
+        agregar.appendChild(inputCantidad);
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar);
+        contenido.appendChild(row);
+
+    });
+}
+
+function agregarPlatillo(producto) {
+
+}
